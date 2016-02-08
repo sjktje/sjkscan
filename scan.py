321b7d7aa2e6f1a7d7262e7933ecb25a355a3471
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-08 21:56:00 +0100

    Try it out

--- scan.py
+++ scan.py
@@ -101,7 +101,14 @@ def main():
     output_dir = 'output'
     scan(output_dir)
 
+    for f in os.scandir(output_dir):
+        if not f.is_file():
+            next
+        remove_if_blank(os.path.join(output_dir, f.name))
+
+
     rotate_all_images_in_dir(output_dir, 180)
+
 
 if __name__ == '__main__':
     main()
