40226c6385b31513fec96fd2cb0802c4504ca1df
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-08 18:29:05 +0100

    Correct docstring for rotate_image

--- scan.py
+++ scan.py
@@ -4,7 +4,7 @@
 from wand.image import Image
 
 def rotate_image(filename, degrees):
-    """Rotates filename degrees degrees."""
+    """Rotate filename given amount of degrees."""
     with Image(filename=filename) as image:
         with image.clone() as rotated:
             rotated.rotate(degrees)
