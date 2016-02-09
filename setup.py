e4cd4ebaf1ef675d10509139903d4b21c87391bf
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-09 02:32:18 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='sjkscan',
+    version='0.1.1a',
+    packages=['sjkscan'],
+    author=u'Svante Kvarnström',
+    author_email='sjk@sjk.io',
+    license='BSD'
+)
