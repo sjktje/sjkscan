a66391ef4a7a8407337cbd61a58ef5a1945c0ecd
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-09 01:02:27 +0100

    Remove main() from module

--- sjkscan/sjkscan.py
+++ sjkscan/sjkscan.py
@@ -114,19 +114,3 @@ def remove_if_blank(filename):
         os.remove(filename)
 
 
-def main():
-    """TODO: docstring"""
-    output_dir = 'output'
-    scan(output_dir)
-
-    for f in os.scandir(output_dir):
-        if not f.is_file():
-            next
-        remove_if_blank(os.path.join(output_dir, f.name))
-
-
-    rotate_all_images_in_dir(output_dir, 180)
-
-
-if __name__ == '__main__':
-    main()
