79a10691ca64ce3b47abfd0f5bf5240ee94d907c
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-25 01:12:57 +0100

    Don't automatically git flow release finish

--- update_version.sh
+++ update_version.sh
@@ -2,4 +2,5 @@
 
 git flow release start v$1
 sed -i -e "s/__version__ = '.*'/__version__ = '$1'/g" sjkscan/__init__.py
-git flow release finish v$1
+echo "Now run tests etc, and then:"
+echo "git flow release finish v$1"
