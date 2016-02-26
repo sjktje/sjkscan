af9adc93c7357015f62c9091326081b080d719be
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-26 20:43:36 +0100

    Add init_logging()

---
+++ sjkscan/logging.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def init_logging():
+    """Initialise logging.
+
+    Set up basic logging.
+    """
+    logging.basicConfig(
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        format='%(asctime)s %(message)s',
+    )
+    logging.debug('Initialising logging')
