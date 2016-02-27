b614f93ed94ea64c9555d3f6dfc0ca89262c2d81
Svante Kvarnström <sjk@ankeborg.nu>
2016-02-27 10:55:24 +0100

    Fix update_version

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 git flow release start v$1
-sed -i "s/__version__ = '.*'/__version__ = '$1'/g" sjkscan/__init__.py
+sed -e "s/__version__ = '.*'/__version__ = '$1'/g" -i sjkscan/__init__.py
 echo "Now run tests etc, and then:"
 echo "git flow release finish v$1"
